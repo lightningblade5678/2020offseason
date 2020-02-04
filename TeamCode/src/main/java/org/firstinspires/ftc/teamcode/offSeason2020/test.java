da2700dc64554fcd42bcefeee69cd769b5904ec5
lightningblade5678 <lightningblade5678@gmail.com>
2020-02-03 20:53:45 -0500

    Update test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/test.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.offSeason2020;
 
+import com.qualcomm.robotcore.*;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
