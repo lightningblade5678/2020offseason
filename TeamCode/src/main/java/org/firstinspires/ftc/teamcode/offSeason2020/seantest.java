a24600d4a9fd5ee18778e3af58bdbd752dd9994d
Sean Noh <43914141+Seancode4@users.noreply.github.com>
2020-02-10 18:09:04 -0500

    t
    this works probably

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/seantest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.offSeason2020;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+
+public class seantest extends OpMode{
+    public DcMotor arm;
+
+    @Override
+
+    public void init() {
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    public void loop() {
+        if (gamepad1.right_stick_y > 0.2 || gamepad1.right_stick_y < -0.2) {
+            arm.setPower(gamepad1.right_stick_y);
+        }
+    }
+
+}
