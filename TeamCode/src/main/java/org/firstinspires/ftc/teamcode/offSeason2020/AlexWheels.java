835606240f1ff994634cf06d480fa9532d0e7425
Zhentarim-Cloak <55816863+Zhentarim-Cloak@users.noreply.github.com>
2020-02-19 20:12:49 -0500

    1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/AlexWheels.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.offSeason2020;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp
+public class AlexWheels extends OpMode {
+
+    private DcMotor works;
+
+    @Override
+    public void init() {
+
+        works = hardwareMap.get(DcMotor.class, deviceName:"AlexWheels");
+    }
+  public void loop() {
+        if(gamepad1.left_stick_y > 0.2) {
+            works.setPower(1);
+        }
+      if(gamepad1.left_stick_y > -0.2) {
+          works.setPower(-1);
+      }
+      }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
