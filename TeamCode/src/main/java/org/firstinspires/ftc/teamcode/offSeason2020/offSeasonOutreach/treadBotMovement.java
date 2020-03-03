cb38ec9a5ee9afd1157aa131d5b67a82316594e4
Zhentarim-Cloak <55816863+Zhentarim-Cloak@users.noreply.github.com>
2020-03-02 19:15:38 -0500

    1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/offSeasonOutreach/treadBotMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/offSeasonOutreach/treadBotMovement.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode.offSeason2020.offSeasonOutreach;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class treadBotMovement {
+import org.firstinspires.ftc.robotcore.internal.network.DeviceNameListener;
+
+@TeleOp
+public class treadBotMovement extends OpMode {
+
+    private DcMotor left, right;
+
+    public void init() {
+        right = hardwareMap.get(DcMotor.class, "treadBotMovement");
+        left = hardwareMap.get(DcMotor.class, "treadBotMovement");
+
+    }
+
+    public void loop() {
+        if (gamepad1.left_stick_y > 0.2) {
+            right.setPower(1);
+            left.setPower(1);
+        }
+        if (gamepad1.left_stick_y < -0.2) {
+            right.setPower(-1);
+            left.setPower(-1);
+        }
+        if (gamepad1.left_stick_x > 0.2) {
+            right.setPower(-1);
+            left.setPower(1);
+        }
+        if (gamepad1.left_stick_x < -0.2) {
+            right.setPower(1);
+            left.setPower(-1);
+        }
+
+    }
 }
+
