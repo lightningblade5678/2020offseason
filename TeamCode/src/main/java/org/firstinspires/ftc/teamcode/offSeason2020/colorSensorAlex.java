976a44e6e79728cb9b6c04f60bec6a92e9791668
Zhentarim-Cloak <55816863+Zhentarim-Cloak@users.noreply.github.com>
2020-03-09 19:30:07 -0400

    1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/colorSensorAlex.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/offSeason2020/colorSensorAlex.java
@@ -6,20 +6,18 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 public class colorSensorAlex extends LinearOpMode {
 
     @Override
-    public void runOpMode() throws InterruptedException {
-
-
-        ColorSensor color_sensor;
-        public void init () {
-            color_sensor = hardwareMap.colorSensor.get("color");
-
-        }
-
-        @Override
-        public void loop () {
-            color_sensor.argb();
-        }
-
-
+    public void runOpMode() {
+        color_sensor.argb();
+        color_sensor.argb();
     }
+        ColorSensor color_sensor;
+         {
+            color_sensor = hardwareMap.colorSensor.get("color");
+        }
+
 }
+
+
+
+
+
